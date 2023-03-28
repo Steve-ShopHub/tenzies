67df804fcdaa6bcf3699cb5a6907dbe1934fb643
Steve <steve@tryshophub.com>
2023-03-28 19:01:41 +0100

    Enable freeze functionality for dice

--- tenzies/src/components/App.jsx
+++ tenzies/src/components/App.jsx
@@ -2,52 +2,13 @@
 import { useEffect, useState } from 'react'
 import '/src/global.css'
 import Main from './Main'
+import nextId from "react-id-generator";
 
 function App() {
 
   const [allDice, setAllDice] = useState([])
 
-//   function allNewDice() {
-      
-//     setAllDice(() => {
-      
-//       let dieArr = [];
-//       for (let i = 0; i < 10; i++) {
-//         if (allDice === []) {
-//           const randomNumber = Math.floor(Math.random() * 6) + 1;
-//           dieArr.push({
-//                     number: randomNumber,
-//                     freeze: false
-//           });
-//           return dieArr;
-//       }
-//       else if (allDice !== []) {
-//         const randomNumber = Math.floor(Math.random() * 6) + 1;
-//         setAllDice (prevAllDice => function() {
-//           return (
-//             prevAllDice.map(prevDie => {
-//               if (prevDie.freeze === true) {
-//                 return {
-//                   number: prevDie.number,
-//                   freeze: true            
-//                 }
-//               } else return {
-//                 number: randomNumber,
-//                 freeze: false            
-//               } 
-//             }
-//               ))
-//           }
-//           )
-//         }
-          
-          
 
-//     }
-
-      
-//   })
-// }
 
 
 function allNewDice() {
@@ -56,22 +17,26 @@ function allNewDice() {
     for (let i = 0; i < 10; i++) {
       const randomNumber = Math.floor(Math.random() * 6) + 1;
       dieArr.push({
+        id: `id${dieArr.length}`,
         number: randomNumber,
         freeze: false,
       });
     }
     setAllDice(dieArr);
+    console.log(dieArr);
   } else {
     setAllDice((prevAllDice) =>
       prevAllDice.map((prevDie) => {
         if (prevDie.freeze === true) {
           return {
+            id: prevDie.id,
             number: prevDie.number,
             freeze: true,
           };
         } else {
           const randomNumber = Math.floor(Math.random() * 6) + 1;
           return {
+            id: prevDie.id,
             number: randomNumber,
             freeze: false,
           };
@@ -95,6 +60,7 @@ function allNewDice() {
       <Main 
       dice={allDice}
       newDice={allNewDice}
+      setAllDice={setAllDice}
       
       />
     </div>
--- tenzies/src/components/Main.jsx
+++ tenzies/src/components/Main.jsx
@@ -5,18 +5,51 @@ export default function Main(props) {
     
 
     
-    function freezeDie() {
-        console.log('Clicked die')
+    function freezeDie(event) {
+
+
+        // // console.log('Clicked die')
+        // console.log(event)
+        // console.log(event.target)
+        // console.log(event.target.id)
+        // console.log(event.target.key)
+
+        // // console.log(event.target.name)
+        // console.log(event.target.value)
+
+
+        props.setAllDice((prevAllDice) =>
+            prevAllDice.map((prevDie) => {
+            if (prevDie.id === event.target.id) {
+                console.log(prevDie.id + ' ' + event.target.id )
+                
+                return {
+                    id: prevDie.id,
+                    number: prevDie.number,
+                    freeze: (!prevDie.freeze),
+                };
+            } else {
+                // console.log(prevDie.id + ' ' + event.target.id )
+            return {
+                ...prevDie
+            }
+        }
+
+        }
+            ))
+
+
     }
+
 
     const diceElements = props.dice.map((die, index) => 
     <div 
         className="die" 
-        id={"die-" + index.toString()} 
+        id={"id" + index.toString()} 
         value={props.dice[index].number} 
-        key={index} 
+        key={props.dice[index].id} 
         onClick={(event) => freezeDie(event)}>{props.dice[index].number}
-        
+
     </div>)
 
 
